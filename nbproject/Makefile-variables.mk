7fb2fa915a126d08cd8238a848c24d7194f183ca
Mike Schoonover <mike@skoonie.com>
2016-03-12 18:04:49 -0600

    Updated project files.
    Signed-off-by: Mike Schoonover <mike@skoonie.com>

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=EDM-SWITCH-PIC-3.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/EDM-SWITCH-PIC-3.production.hex
+CND_ARTIFACT_NAME_default=03_EDM-Switch-PIC-3.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/03_EDM-Switch-PIC-3.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=edm-switch-pic-3.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/edm-switch-pic-3.tar
+CND_PACKAGE_NAME_default=03edm-switch-pic-3.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/03edm-switch-pic-3.tar
